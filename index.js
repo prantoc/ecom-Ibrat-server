a917994ecb9f1ebd043323f1e017479582033be5
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-20 23:30:08 +0600

    required file setup

---
+++ index.js
@@ -0,0 +1,26 @@
+//# all required 
+const express = require('express')
+const app = express()
+var cors = require('cors')
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+//#port setup
+const port = process.env.PORT || 5000
+//# used cors
+app.use(cors())
+//# json req body parser 
+app.use(express.json())
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
